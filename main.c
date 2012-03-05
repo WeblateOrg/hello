5f81cb21ff1b9f5929e3b25bfcdb6ae62a9c08c0
Michal Čihař <mcihar@suse.cz>
2012-03-05 15:58:18 +0100

    Add plural message

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 int main(void)
 {
+    int count = 1;
     setlocale( LC_ALL, "" );
     bindtextdomain( "hello", "/usr/share/locale" );
     textdomain( "hello" );
     printf( gettext( "Hello, world!\n" ) );
+    printf( ngettext( "Orangutan has %d banana.\n", "Orangutan has %d bananas.\n", count ), count );
     exit(0);
 }
